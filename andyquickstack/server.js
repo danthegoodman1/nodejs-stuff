98e6ef264202fae86a318b302f6bb97b16c107c7
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-03-27 15:42:55 -0400

    Create server.js

---
+++ andyquickstack/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var router = express();
+var server = http.createServer(router);
+router.use(bodyParser.json());
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+
+router.get('/:collection', function(req, res){
+  var collection = req.params.collection;
+})
+.get('/:collection/:id', function(req, res){
+  var collection = req.params.collection;
+  var id = req.params.id;
+})
+.post('/:collection', function(req, res){
+  
+})
+.put('/:collection/:id', function(req, res){
+  var collection = req.params.collection;
+  var id = req.params.id;
+})
+.delete('/:collection/:id', function(req, res){
+  var collection = req.params.collection;
+  var id = req.params.id;
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Quick stack backend listening at", addr.address + ":" + addr.port);
+});
